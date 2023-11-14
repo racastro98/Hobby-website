6a88f4293afc1cc2b818e47b4cfd0b4ba9bd62c4
Ev-13 <138253637+Ev-13@users.noreply.github.com>
2023-11-14 19:28:53 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var message = "main.js connected successfully";
+console.log(message);
+
+document.addEventListener(`DOMContentLoaded`, function() {
+    const button = document.getElementById()
+});
