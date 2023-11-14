6fd3b3b7669bf83f19db59bd8cd71e08643d3f90
Ev-13 <138253637+Ev-13@users.noreply.github.com>
2023-11-14 19:39:27 +0000

    Deleted Evie's first uploading attempt

--- main.js
+++
@@ -1,6 +0,0 @@
-var message = "main.js connected successfully";
-console.log(message);
-
-document.addEventListener(`DOMContentLoaded`, function() {
-    const button = document.getElementById()
-});
