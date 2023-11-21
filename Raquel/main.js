f59a7e8d6f6460301df35b925514d605c5302cc7
racastro98 <147542698+racastro98@users.noreply.github.com>
2023-11-21 11:37:51 +0000

    added javascript

---
+++ Raquel/main.js
@@ -0,0 +1,10 @@
+var message = "main.js connected successfully";
+console.log(message);
+
+document.addEventListener(`DOMContentLoaded`, function() {
+    const button = document.getElementById()
+});
+
+function submitFunction(){
+    alert("Form submitted!");
+}
